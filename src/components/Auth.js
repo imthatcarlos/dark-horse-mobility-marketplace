7918134d239d7204911e5c885f908c7e30b149c8
Carlos Beltran <imthatcarlos@gmail.com>
2020-07-15 00:06:52 -0400

    wip - create profile popover w/ bucket get/create

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -9,13 +9,21 @@ import {
   Popover,
   TextField,
   Paper,
-  Typography
+  Typography,
+  InputLabel,
+  Select,
+  MenuItem
 } from '@material-ui/core';
 import PeopleIcon from '@material-ui/icons/People';
 import Cookies from 'js-cookie';
 import { isEmpty } from 'lodash/lang';
+import { find } from 'lodash/collection';
+
+import Title from './Title';
 
 const SPACE = 'mobility-marketplace';
+const CATEGORIES = ['mobility', 'fashion', 'sports', 'coffee'];
+const COUNTRIES = ['United States', 'Italy', 'France', 'Spain'];
 
 export default function Auth(props) {
   const { web3, spaceClient } = props;
@@ -23,10 +31,13 @@ export default function Auth(props) {
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClickOpen = (event) => { setAnchorEl(event.currentTarget); setOpenModal(true); };
   const handleClose = () => { setAnchorEl(null); setOpenModal(false); }
-  const [profile, setProfile] = useState(null);
+  const [identity, setIdentity] = useState(null);
   const [newUser, setNewUser] = useState(null);
+  const [buckets, setBuckets] = useState(null);
   const [error, setError] = useState(null);
   const inputUsername = useRef();
+  const inputOrg = useRef();
+  const [inputCategory, setInputCategory] = useState('');
 
   const getIdentity = async () => {
     const _username = Cookies.get(`${SPACE}-username`);
@@ -36,19 +47,31 @@ export default function Auth(props) {
       try {
         const res = await spaceClient.getIdentityByUsername({ username: _username });
         console.log(res.getIdentity());
-        setProfile(res.getIdentity());
+        setIdentity(res.getIdentity());
       } catch (error) {
-        setError('username not exist');
+        setError('username does not exist');
       }
     }
   };
 
+  const getBuckets = async () => {
+    const res = await spaceClient.listBuckets();
+    const buckets = res.getBucketsList();
+    console.log(buckets);
+
+    setBuckets(buckets);
+    // have they initialized a private identity?
+    setNewUser(!find(buckets, (b) => b.getName() === `${SPACE}/profile`));
+  }
+
   useEffect(() => {
     // init
-    if (profile === null) {
-      getIdentity();
-    }
-  }, [newUser, error]);
+    // if (identity === null) {
+    //   getIdentity();
+    // } else {
+    //   getBuckets();
+    // }
+  }, [newUser, error, identity]);
 
   const auth = async () => {
     const username = inputUsername.current.value;
@@ -57,15 +80,28 @@ export default function Auth(props) {
     try {
       const res = await spaceClient.getIdentityByUsername({ username });
       Cookies.set(`${SPACE}-username`, username);
-      setProfile(res.getIdentity());
+      setIdentity(res.getIdentity());
     } catch (error) {
       try {
         await spaceClient.createUsernameAndEmail({ username });
-        console.log('username created!')
+        console.log('username created!');
         Cookies.set(`${SPACE}-username`, username);
       } catch (error) {
         setError('wrong username');
       }
+    }
+  };
+
+  const createProfile = async () => {
+    try {
+      const res = await spaceClient.createBucket({ slug: `${SPACE}/profile` });
+      const bucket = res.getBucket();
+
+      console.log(bucket.getName());
+
+      setBuckets([bucket]);
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -95,7 +131,7 @@ export default function Auth(props) {
               align="center"
             >
               <Grid item xs={12} >
-                Login to {SPACE}
+                <Title>Login</Title>
               </Grid>
               <Grid item xs={8}>
                  <TextField
@@ -117,17 +153,90 @@ export default function Auth(props) {
     )
   };
 
+  const createProfilePopover = () => {
+    return (
+      <div>
+        <Button aria-describedby="profile-popover" onClick={handleClickOpen}>{identity ? identity.array[2] : null}</Button>
+        <Popover
+          id={'profile-popover'}
+          open={openModal}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          className="popover"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <Paper style={{ padding: 20, width: 400, height: 200 }}>
+            <Grid
+              spacing={1}
+              container
+              align="center"
+            >
+              <Grid item xs={12} >
+                <Title>Advertiser Profile</Title>
+              </Grid>
+              <Grid item xs={6}>
+                 <TextField
+                   error={error !== null}
+                   helperText={error}
+                   id="profile-org"
+                   label="Organization"
+                   ref={inputOrg}
+                   onChange={e => inputOrg.current.value = e.target.value}
+                  />
+              </Grid>
+              <Grid item xs={6}>
+                <InputLabel id="profile-category">Category</InputLabel>
+                <Select
+                   labelId="profile-category"
+                   id="profile-category-input"
+                   value={inputCategory}
+                   onChange={e => setInputCategory(e.target.value)}
+                   style={{ minWidth: 140 }}
+                >
+                  {
+                    CATEGORIES.map((c) => <MenuItem value={c}>{c}</MenuItem>)
+                  }
+                </Select>
+              </Grid>
+              <Grid item xs={8} />
+              <Grid item xs={4}>
+                 <Button onClick={createProfile} style={{ marginTop: 10 }}>Create</Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Popover>
+      </div>
+    )
+  };
+
+  const showBadgeNotif = (
+    (isEmpty(identity) && newUser)
+      || (identity && newUser)
+  );
+
   return (
-    <Badge color="secondary" >
+    <Badge color="secondary" badgeContent={showBadgeNotif ? 1 : 0}>
       <PeopleIcon style={{ marginTop: 5 }}/>
 
       <div style={{
         alignItems: 'center',
       }}>
         {
-          isEmpty(profile) && newUser === true
+          isEmpty(identity) && newUser === true
             ? popover()
-            : <div style={{ marginTop: 8, marginLeft: 10 }}>{profile ? profile.array[2] : null}</div>
+            : null
+        }
+        {
+          identity && newUser === true
+            ? createProfilePopover()
+            : null
         }
       </div>
     </Badge>
